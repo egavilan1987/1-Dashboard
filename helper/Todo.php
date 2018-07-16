049c7ffec588d27fd0cd9c152b9d02763bcc883f
egavilan1987 <enmanuelgc@gmail.com>
2018-07-16 17:38:26 -0400

    Update Todo.php

--- helper/Todo.php
+++ helper/Todo.php
@@ -23,3 +23,21 @@ Espanol
 *Lectura
 *Escritura
 *1 Capitulo de la Biblia
+---------------------------------------------------
+email
+employee
+gender
+password
+password_confirmation
+
+check_email();
+check_employee();
+check_gender();
+check_password();
+check_password_confirmation();
+
+<div id="email_error_message" style="color:red"></div>
+<div id="employee_error_message" style="color:red"></div>
+<div id="gender_error_message" style="color:red"></div>
+<div id="password_error_message" style="color:red"></div>
+<div id="password_confirmation_error_message" style="color:red"></div>
