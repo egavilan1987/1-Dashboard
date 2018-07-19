f80cd5964edf153b19e3fcceba67ce7e1967a9fb
egavilan1987 <enmanuelgc@gmail.com>
2018-07-19 07:22:40 -0400

    7-19-2018 (Morning)

--- helper/Todo.php
+++ helper/Todo.php
@@ -5,13 +5,17 @@ BECAUSE TODAY WE ARE USING A NEW SYSTEM IN MY JOBPLACE MAYBE THERE WILL NOT BE T
 (1) FINISH THE RESUME.
 
 Coding
-	*Check about how to change the field to red color.
+	
 	*Study the add users code many times.
 	*Check the whole code.
 	*Study Sales and Inventory Sales code.
 	*IF POSIBLE CODE.
 
 
+RESEACH
+	*Loading button
+	*Check about how to change the field to red color.
+	
 --------------------------------------------
 On my Job To Do List
 1-English
